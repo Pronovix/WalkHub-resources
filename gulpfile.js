ef84fcf05b65ea3a656a3ec9a81e9cb233be5cde
Tamás Demeter-Haludka <yorirou@gmail.com>
2014-06-13 14:55:06 +0200

    Fixes syntax error in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@
       "src/javascript/noconflict.js",
       "src/javascript/jquery.cookie.js",
       "src/javascript/URI.js",
-      "src/javascript/modernizr.mq.js",
+      "src/javascript/modernizr.mq.js"
     ],
     non_vendor_scripts: [
       "src/javascript/polyfill.js",
